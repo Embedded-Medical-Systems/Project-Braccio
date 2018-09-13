82978220e7ac94e1969c08a316fae5254255a406
wraftus <wrraftus@uwaterloo.ca>
2018-09-13 00:52:45 -0400

    motor control

--- nanoCode/nanoCode.ino
+++ nanoCode/nanoCode.ino
@@ -1,6 +1,14 @@
 #include <SPI.h>
 
-int slaveSelect A7;
+byte slaveSelect A7;
+byte transForward1 = D4;
+byte transForward2 = D6;
+byte transBack1 = D3;
+byte transBack2 = D5;
+byte rotForward1 = D8;
+byte rotForward2 = D10;
+byte rotBack1 = D7;
+byte rotBack2 = D9;
 
 int readCounter = -1;
 const byte DIRECTIONS_INDEX = 0;
@@ -12,8 +20,16 @@ volatile int transSpeed = 0;
 volatile int rotSPeed = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(slaveSelect, OUTPUT);
+  pinMode(slaveSelect, INPUT);
+  pinMode(transForward1, OUTPUT);
+  pinMode(transForward2, OUTPUT);
+  pinMode(transBack1, OUTPUT);
+  pinMode(transBack2, OUTPUT);
+  pinMode(rotForward1, OUTPUT);
+  pinMode(rotForward2, OUTPUT);
+  pinMode(rotBack1, OUTPUT)
+  pinMode(rotBack2, OUTPUT);
+  
   SPI.begin();
   SPI.setBitOrder(MSBFIRST);
   SPCR |= _BV(SPE);
@@ -35,5 +51,19 @@ ISR (SPI_STC_vect) {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(transFront1, direction & 2 ? HIGH : LOW);
+  digitalWrite(transFront2, direction & 2 ? HIGH : LOW);
+
+  digitalWrite(transBack1, direction & 2 ? LOW : HIGH);
+  digitalWrite(transBack2, direction & 2 ? LOW : HIGH);
+
+  analogWrite(direction & 2 ? transBack1 : transBack2, transSpeed & 0xFF);
+
+  digitalWrite(rotFront1, direction & 1 ? HIGH : LOW);
+  digitalWrite(rotFront2, direction & 1 ? HIGH : LOW);
+
+  digitalWrite(rotBack1, direction & 1 ? LOW : HIGH);
+  digitalWrite(rotBack2, direction & 1 ? LOW : HIGH);
+
+  analogWrite(direction & 1 ? rotBack1 : rotBack2, rotSpeed & 0xFF);
 }
