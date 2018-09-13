14eb6a29370392a6a5f11c4e99dad8523278e6ac
wraftus <wrraftus@uwaterloo.ca>
2018-09-13 15:46:21 -0400

    fixed some syntax and logic erros

--- nanoCode/nanoCode.ino
+++ nanoCode/nanoCode.ino
@@ -1,23 +1,23 @@
 #include <SPI.h>
 
-byte slaveSelect A7;
-byte transForward1 = D4;
-byte transForward2 = D6;
-byte transBack1 = D3;
-byte transBack2 = D5;
-byte rotForward1 = D8;
-byte rotForward2 = D10;
-byte rotBack1 = D7;
-byte rotBack2 = D9;
+byte slaveSelect = A7;
+byte transForward1 = 4;
+byte transForward2 = 6;
+byte transBack1 = 3;
+byte transBack2 = 5;
+byte rotForward1 = 8;
+byte rotForward2 = 10;
+byte rotBack1 = 7;
+byte rotBack2 = 9;
 
 int readCounter = -1;
 const byte DIRECTIONS_INDEX = 0;
 const byte TRANS_INDEX = 1;
 const byte ROT_INDEX = 2;
 
-volatile int directions = 3;
-volatile int transSpeed = 0;
-volatile int rotSPeed = 0;
+volatile byte directions = 3;
+volatile byte transSpeed = 0;
+volatile byte rotSpeed = 0;
 
 void setup() {
   pinMode(slaveSelect, INPUT);
@@ -27,7 +27,7 @@ void setup() {
   pinMode(transBack2, OUTPUT);
   pinMode(rotForward1, OUTPUT);
   pinMode(rotForward2, OUTPUT);
-  pinMode(rotBack1, OUTPUT)
+  pinMode(rotBack1, OUTPUT);
   pinMode(rotBack2, OUTPUT);
   
   SPI.begin();
@@ -40,7 +40,7 @@ ISR (SPI_STC_vect) {
   if (++readCounter == DIRECTIONS_INDEX){
     directions = SPDR;
   }
-  else if (readcounter == TRANS_INDEX){
+  else if (readCounter == TRANS_INDEX){
     transSpeed = SPDR;
   }
   else {
@@ -51,19 +51,19 @@ ISR (SPI_STC_vect) {
 }
 
 void loop() {
-  digitalWrite(transFront1, direction & 2 ? HIGH : LOW);
-  digitalWrite(transFront2, direction & 2 ? HIGH : LOW);
+  digitalWrite(transForward1, directions & 2 ? HIGH : LOW);
+  digitalWrite(transForward2, directions & 2 ? HIGH : LOW);
 
-  digitalWrite(transBack1, direction & 2 ? LOW : HIGH);
-  digitalWrite(transBack2, direction & 2 ? LOW : HIGH);
+  digitalWrite(transBack1, directions & 2 ? LOW : HIGH);
+  digitalWrite(transBack2, directions & 2 ? LOW : HIGH);
 
-  analogWrite(direction & 2 ? transBack1 : transBack2, transSpeed & 0xFF);
+  analogWrite(directions & 2 ? transForward2 : transBack2, transSpeed & 0xff);
 
-  digitalWrite(rotFront1, direction & 1 ? HIGH : LOW);
-  digitalWrite(rotFront2, direction & 1 ? HIGH : LOW);
+  digitalWrite(rotForward1, directions & 1 ? HIGH : LOW);
+  digitalWrite(rotForward2, directions & 1 ? HIGH : LOW);
 
-  digitalWrite(rotBack1, direction & 1 ? LOW : HIGH);
-  digitalWrite(rotBack2, direction & 1 ? LOW : HIGH);
+  digitalWrite(rotBack1, directions & 1 ? LOW : HIGH);
+  digitalWrite(rotBack2, directions & 1 ? LOW : HIGH);
 
-  analogWrite(direction & 1 ? rotBack1 : rotBack2, rotSpeed & 0xFF);
+  analogWrite(directions & 1 ? rotForward2 : rotBack2, rotSpeed & 0xff);
 }
