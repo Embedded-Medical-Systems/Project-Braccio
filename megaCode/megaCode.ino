ab27dbf93d20c127391086643400a001e9e19281
wraftus <wrraftus@uwaterloo.ca>
2018-09-14 02:25:56 -0400

    pid???

--- megaCode/megaCode.ino
+++ megaCode/megaCode.ino
@@ -1,45 +1,72 @@
 #include <SPI.h>
 
 struct Cart {
-  byte slaveSelect;
-  byte linPotPin;
-  byte rot1Pin;
-  byte rot2Pin;
-  float kp;
-  float kd;
-  float prevError;
+  const byte slaveSelect;
+  const byte linPotPin;
+  const byte rot1Pin;
+  const byte rot2Pin;
+  const float kpTrans;
+  const float kdTrans;
+  const float kpRot;
+  const float kdRot;
+  float prevErrorTrans;
+  float prevErrorRot;
 };
 
 const float R = 1;
 
 const float linPotFact = 100 / 1023.f;
 
-const Cart leftFrontCart = {
+Cart leftFrontCart = {
   .slaveSelect = 2,
   .linPotPin = A0,
   .rot1Pin = A1,
-  .rot2Pin = A2
+  .rot2Pin = A2,
+  .kpTrans = 0,
+  .kdTrans = 0,
+  .kpRot = 0,
+  .kdRot = 0,
+  .prevErrorTrans = 0,
+  .prevErrorRot = 0
 };
 
-const Cart rightFrontCart = {
+Cart rightFrontCart = {
   .slaveSelect = 3,
   .linPotPin = A3,
   .rot1Pin = A4,
-  .rot2Pin = A5
+  .rot2Pin = A5,
+  .kpTrans = 0,
+  .kdTrans = 0,
+  .kpRot = 0,
+  .kdRot = 0,
+  .prevErrorTrans = 0,
+  .prevErrorRot = 0
 };
 
-const Cart leftBackCart = {
+Cart leftBackCart = {
   .slaveSelect = 4,
   .linPotPin = A6,
   .rot1Pin = A7,
-  .rot2Pin = A8
+  .rot2Pin = A8,
+  .kpTrans = 0,
+  .kdTrans = 0,
+  .kpRot = 0,
+  .kdRot = 0,
+  .prevErrorTrans = 0,
+  .prevErrorRot = 0
 };
 
-const Cart rightBackCart = {
+Cart rightBackCart = {
   .slaveSelect = 5,
   .linPotPin = A9,
   .rot1Pin = A10,
-  .rot2Pin = A11
+  .rot2Pin = A11,
+  .kpTrans = 0,
+  .kdTrans = 0,
+  .kpRot = 0,
+  .kdRot = 0,
+  .prevErrorTrans = 0,
+  .prevErrorRot = 0
 };
 
 void setCartPins(Cart cart) {
@@ -61,13 +88,36 @@ void setMotorSpeed(byte slaveSelect, float transSpeed, float rotSpeed) {
   digitalWrite(slaveSelect, HIGH);
 }
 
-void linearControl(float dist, Cart cart) {
-  //TODO do control loops/ run motors
+float linearControl(float dist, Cart& cart) {
   float error = dist - analogRead(cart.linPotPin) * linPotFact;
 
-  float proportional = error * cart.kp;
-  float derivative = (error - cart.prevError) * cart.kd;
+  float proportional = error * cart.kpTrans;
+  if(cart.prevErrorTrans == 0){
+    cart.prevErrorTrans = error;
+  }
+  float derivative = (error - cart.prevErrorTrans) * cart.kdTrans;
+  cart.prevErrorTrans = error;
 
+  return proportional + derivative;
+
+}
+
+float rotationControl(float theta, Cart& cart) {
+  float currentTheta = readPot(cart.rot1Pin, cart.rot2Pin);
+  float error = theta - currentTheta;
+
+  float proportional = error * cart.kpRot;
+  if(cart.prevErrorRot == 0){
+    cart.prevErrorRot = error;
+  }
+  float derivative = (error - cart.prevErrorRot) * cart.kdRot;
+  cart.prevErrorRot = error;
+
+  return proportional + derivative;
+}
+
+float readPot(const int potPin1, const int potPin2){
+  return 0;
 }
 
 void getControllerPosition(float& x, float& y, float& z) {
@@ -92,6 +142,21 @@ void setup() {
 void loop() {
   //TODO get two (X,Y,Z) positions from vive
   //TODO put them both through physics model
-  //TODO use control loop on physics model
+  float leftd1, rightd1, leftd2, rightd2, leftTheta, rightTheta;
+
+  float leftFrontLin = linearControl(leftd1,leftFrontCart);
+  float leftBackLin = linearControl(leftd2,leftBackCart);
+  float leftFrontRot = rotationControl(leftTheta, leftFrontCart);
+  float leftBackRot = rotationControl(leftTheta, leftBackCart);
+
+  float rightFrontLin = linearControl(rightd1,rightFrontCart);
+  float rightBackLin = linearControl(rightd2,leftBackCart);
+  float rightFrontRot = rotationControl(rightTheta, rightFrontCart);
+  float rightBackRot = rotationControl(rightTheta, leftBackCart);
+  
+  setMotorSpeed(leftFrontCart.slaveSelect, leftFrontLin, leftFrontRot);
+  setMotorSpeed(leftBackCart.slaveSelect, leftBackLin, leftBackRot);
+  setMotorSpeed(rightFrontCart.slaveSelect, rightFrontLin, rightFrontRot);
+  setMotorSpeed(leftBackCart.slaveSelect, rightBackLin, rightBackRot);
 
 }
